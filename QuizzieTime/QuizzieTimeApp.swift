4e1b96385ddf9787bb68ec3a08ab4819997c8ed5
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2023-02-26 20:35:59 +0300

    Initial Commit

---
+++ QuizzieTime/QuizzieTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzieTimeApp.swift
+//  QuizzieTime
+//
+//  Created by Andrei Dokuchaev on 26.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizzieTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
